f8f623e5ce60f2580647955e6a3cd157c329f616
rajeshkumarojha-dev <rajeshkumarojha.dev@gmail.com>
2024-08-26 20:31:43 +0530

    python program

--- length&breadth/(a+b)^2.py
+++ length&breadth/(a+b)^2.py
@@ -1,7 +1,10 @@
 a = int(input("enter value of a: "))
 b = int(input("enter value of b: "))
 
-# c=a**2+2*a*b+b**2
+c=a**2+2*a*b+b**2
 
 print(f"({a},{b})={a**2+2*a*b+b**2}")
+print('({}+{})**2 value is: {}'.format(a,b,c))
+
+
 
